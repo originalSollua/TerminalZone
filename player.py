e28755c07d43c15d5e97cf0ee4bb165b8d8a9433
Nicholas Marasco <marascnd197@potsdam.edu>
2014-10-08 11:46:54 -0400

    Added gravity physics to the player

--- player.py
+++ player.py
@@ -7,7 +7,7 @@
 #        Brandon Williams
 #        Jeremy Rose
 #
-# Last modification: 10/06/14
+# Last modification: 10/08/14
 #
 # Description: Player class
 # Designed to hold all information about the player.
@@ -24,6 +24,7 @@ from camMov import CameraMovement
 
 from panda3d.core import CollisionNode, CollisionSphere, CollisionRay, CollisionHandlerQueue
 from panda3d.core import NodePath, BitMask32
+from panda3d.physics import ActorNode, ForceNode, LinearVectorForce
 
 class Player(object):
     #using this to be our player
@@ -31,29 +32,26 @@ class Player(object):
     #have to tie the camera to this
     #game manager ->player ->camera as far as instantiating goes
 
-    #player variables
-    #camera stuck in players head
-    global	cam
-    global	cm
-    
     def __init__(self):
-        self.node = NodePath('player')
-        self.node.reparentTo(render)
-        self.node.setPos(0,15,0)
-        self.node.setScale(1.0)
+        base.enableParticles()
+        self.playerNode = NodePath('player')
+        self.playerNode.reparentTo(render)
+        self.playerNode.setPos(0,15,0)
+        self.playerNode.setScale(1.0)
         cameraModel = loader.loadModel("models/camera")
-        cameraModel.reparentTo(self.node)
+        cameraModel.reparentTo(self.playerNode)
        	cameraModel.hide()
-        cameraModel.setPos(0,15,0)
+        cameraModel.setPos(0,0,0)
         base.taskMgr.add(CameraMovement(cameraModel).cameraControl, "cameraControl")
         self.createColision()
+        self.createGravity()
 
     def createColision(self):
         colNode = CollisionNode("player")
         colNode.addSolid(CollisionSphere(0, 15, 0, 20))
-        solid = self.node.attachNewNode(colNode)
+        solid = self.playerNode.attachNewNode(colNode)
         base.cTrav.addCollider(solid, base.pusher)
-        base.pusher.addCollider(solid, self.node, base.drive.node())
+        base.pusher.addCollider(solid, self.playerNode, base.drive.node())
         #player vs floor
         ray = CollisionRay()
         #use camera model position
@@ -63,6 +61,24 @@ class Player(object):
         colNode.addSolid(ray)
         colNode.setFromCollideMask(BitMask32.bit(0))
         colNode.setIntoCollideMask(BitMask32.allOff())
-        solid = self.node.attachNewNode(colNode)
+        solid = self.playerNode.attachNewNode(colNode)
         self.nodeGroundHandler = CollisionHandlerQueue()
         base.cTrav.addCollider(solid, self.nodeGroundHandler)
+
+    def createGravity(self):
+        # Create the Nodes to monitor physics
+        physicsNode = NodePath("physics")
+        physicsNode.reparentTo(render)
+        actNode = ActorNode("player-physics")
+        actNodeParent = physicsNode.attachNewNode(actNode)
+        # Add physics node to the manager
+        base.physicsMgr.attachPhysicalNode(actNode)
+        self.playerNode.reparentTo(actNodeParent)
+        actNode.getPhysicsObject().setMass(90)
+        # Create gravity nodes and forces
+        gravityFN = ForceNode("dat-grav")
+        gravityFNP = render.attachNewNode(gravityFN)
+        gravityForce = LinearVectorForce(0,0,-9.81)
+        gravityFN.addForce(gravityForce)
+        # Add gravity to the manager
+        base.physicsMgr.addLinearForce(gravityForce)
