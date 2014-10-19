b84b52c07061a08cd4888350699443251b8a3e11
pryoreb197 <pryoreb197@potsdam.edu>
2014-10-19 17:37:09 -0400

    projectile enemy interaction almost done. need to despawn enemies next.

--- enemy.py
+++ enemy.py
@@ -16,16 +16,29 @@
 #======================================================================#
 
 from direct.actor.Actor import Actor
-
+from panda3d.core import CollisionNode, CollisionSphere, CollisionTube, NodePath
 
 class Enemy(object):
 
     def __init__(self, model):
-        
+        self.enemyNode = NodePath('enemy')
+        self.enemyNode.reparentTo(base.render)
         # Load the enemy model, set the scale, and add to render
         self.enemy = Actor(model)
         self.enemy.setScale(0.2,0.2,0.2)
-        self.enemy.reparentTo(base.render)
+        self.enemy.reparentTo(self.enemyNode)
+        
+        
+        xTop = self.enemy.getX()
+        yTop = self.enemy.getY()
+        zTop = self.enemy.getZ()-15
+        xBot = xTop
+        yBot = yTop
+        zBot = zTop-10
+        cs = CollisionTube(xTop, yTop, zTop, xBot, yBot, zBot, 20)
+        cnodepath = self.enemy.attachNewNode(CollisionNode('cnode'))
+        cnodepath.node().addSolid(cs)
+        cnodepath.show()  
 
     def setPos(self, x, y, z):
 		self.enemy.setPos(x, y, z)
--- projectile.py
+++ projectile.py
@@ -14,14 +14,14 @@
 #
 #======================================================================#
 from direct.showbase.DirectObject import DirectObject
-from panda3d.core import NodePath, Vec3
+from panda3d.core import NodePath, Vec3, CollisionNode, CollisionSphere, CollisionTraverser, CollisionHandlerEvent
 from math import sin, cos
 # going to use the system time to calculate when to destroy projectiles
 import time
 #possible depricated library
 from direct.interval.IntervalGlobal import *
 
-class Projectile(object):
+class Projectile(DirectObject):
     creaTime = time.clock()
     dur = 2
     vec = 0
@@ -44,10 +44,20 @@ class Projectile(object):
     	# must calculate unit vector based on direction
         dir = render.getRelativeVector(look, Vec3(0, 1, 0))
     	#speed up or slow down projectiles here
-        dir = dir
+        dir = dir*10
         self.vec = dir
-        #self.delta = time.clock()
-        #self.prevtime = time.clock()
+        
+        #base.cTrav = CollisionTraverser()
+        cs = CollisionSphere(0, 0, 5, 5)
+        cnodepath = self.projectileNode.attachNewNode(CollisionNode('cnode'))
+        cnodepath.node().addSolid(cs)
+        cnodepath.show()
+        self.collHand = CollisionHandlerEvent()
+        self.collHand.addInPattern('into')
+        self.collHand.addOutPattern('outof')
+        base.cTrav.addCollider(cnodepath, self.collHand)
+        self.accept('into', self.hit)
+        
         
         #changing to a move task
 
@@ -59,6 +69,8 @@ class Projectile(object):
     def moveTask(self, task):
         #curtime = time.clock()
         #self.delta = curtime-self.prevtime
+        if self.flag:
+            return task.done
         velx = self.vec.x*self.delta
         vely = self.vec.y*self.delta
         velz = self.vec.z*self.delta
@@ -67,9 +79,16 @@ class Projectile(object):
         z = self.projectileNode.getZ()
         self.projectileNode.setPos(x+velx, y+vely, z+velz)
         #prevtime = time.clock()
+            
+        
         if task.time < self.dur:
             return task.cont
         else:
             self.flag = True
             return task.done
         
+
+    def hit(self, collEntry):
+        print collEntry
+        collEntry.getFromNodePath().getParent().getParent().removeNode()
+        self.flag =  True
