8d1ebe29533ff79c9038d265a82be44676788fd7
Nicholas Marasco <marascnd197@potsdam.edu>
2014-11-15 18:10:49 -0500

    RIP Catch Block 2014-2014, also added Key and Value model and textures, modified weapons.py to support them.

--- weapons.py
+++ weapons.py
@@ -89,73 +89,6 @@ class RecursionRifle(object):
 
         self.gunModel.show()
 
-class CatchBlock(object):
-    
-    time = 0
-    step = False
-    curScale = 0
-
-
-    def __init__(self, camera, id):
-        print "guns init"
-        #Set model and projectile paths
-        self.gunPath = NodePath("gun4")
-        self.gunPath.reparentTo(base.camera)
-        self.gunPath.setPos(1 ,10,-3.5)
-        self.gunModel = loader.loadModel("./resources/gunmodel")
-        self.gunModel.reparentTo(self.gunPath)
-        self.gunModel.setPos(camera,.7,3,-.9)
-        self.gunModel.setHpr(0,180,180)
-        self.gunModel.setColor(1, 1, 1)
-        self.reticle = OnscreenImage("./resources/cbReticle.png")
-        self.reticle.setTransparency(True)
-        self.reticle.setScale(0)
-        base.taskMgr.add(self.animate, "cbReticle")
-
-    def fire(self):
-
-        #Spawn projectile, add it to taskMgr and play sfx
-        proj = CBShield(self.gunPath, base.camera, len(base.projectileList))
-        base.taskMgr.add(proj.placeTask, "place projectile")
-        base.projectileList.append(proj)
-        shotSfx = base.loader.loadSfx("./resources/sounds/blunderbuss.wav")
-        shotSfx.setVolume(.4)
-        shotSfx.play()
-        
-        print "Shots fired: ", len(base.projectileList)
-
-    def animate(self, task):
-    
-        if task.time - self.time  > .05:
-            
-            self.reticle.setScale(self.curScale)
-            if self.curScale < .08 and self.step:
-
-                self.curScale += .001
-                self.reticle.setScale(self.curScale)
-                self.time = task.time
-                if self.curScale >= .08:
-
-                    self.step = False
-            elif self.curScale > .05:
-
-                self.curScale -= .001
-                self.reticle.setScale(self.curScale)
-                self.time = task.time
-                if self.curScale <= .05:
-                    
-                    self.step = True
-
-        return task.cont
-    
-    def hide(self):
-
-        self.gunModel.hide()
-
-    def show(self):
-
-        self.gunModel.show()
-
 #Max Heao Blunderbuss
 class MHB(object):
    
@@ -235,25 +168,30 @@ class KeyValue(object):
     fireLeft = False
 
     def __init__(self, camera, id):
+       
+        #load the other keyValue texture
+        keyTexRed = base.loader.loadTexture("resources/tex/keyValueRed.png")
+        
         print "guns init"
         #Set model and projectile paths
         self.gunPath1 = NodePath("gun")
         self.gunPath1.reparentTo(base.camera)
-        self.gunPath1.setPos(.9 ,10,-3.5)
-        self.gunModel1 = loader.loadModel("./resources/gunmodel")
+        self.gunPath1.setPos(1.2 ,10,-3.5)
+        self.gunModel1 = loader.loadModel("./resources/keyValue")
+        self.gunModel1.setTexture(keyTexRed,1)
         self.gunModel1.reparentTo(self.gunPath1)
-        self.gunModel1.setPos(camera,.7,3,-.9)
-        self.gunModel1.setHpr(0,180,180)
-        self.gunModel1.setColor(255, 0, 0)
+        self.gunModel1.setPos(camera,1.2,4.5,-1.7)
+        self.gunModel1.setHpr(-90,0,0)
+        #self.gunModel1.setColor(255, 0, 0)
 
         self.gunPath2 = NodePath("gun")
         self.gunPath2.reparentTo(base.camera)
-        self.gunPath2.setPos(-.85 ,10,-3.5)
-        self.gunModel2 = loader.loadModel("./resources/gunmodel")
+        self.gunPath2.setPos(-1.2,10,-3.5)
+        self.gunModel2 = loader.loadModel("./resources/keyValue")
         self.gunModel2.reparentTo(self.gunPath2)
-        self.gunModel2.setPos(camera,-.65,3,-.9)
-        self.gunModel2.setHpr(0,180,180)
-        self.gunModel2.setColor(0, 0, 255)
+        self.gunModel2.setPos(camera,-1.2,4.5,-1.7)
+        self.gunModel2.setHpr(-90,0,0)
+        #self.gunModel2.setColor(0, 0, 255)
 
         self.reticle = OnscreenImage("./resources/kvReticle.png")
         self.reticle.setTransparency(True)
